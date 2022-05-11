c90650ea7e45e5f061e15350adda5e27585ae678
aaronvanoordt <83145139+aaronvanoordt@users.noreply.github.com>
2022-05-11 15:53:47 -0500

    Primer avance login
    Login (beta)
    Registrar: Crear.
    Error o exito.

--- app.py
+++ app.py
@@ -1,20 +1,27 @@
 #Imports
 
-from flask import Flask, redirect, render_template, request, jsonify
+from flask import Flask, redirect, render_template, request, jsonify, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask.helpers import url_for
-from datetime import datetime
-
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 #Configuration
 
 app = Flask(__name__)
+app.config['SESSION_TYPE'] = 'memcached'
+app.config['SECRET_KEY'] = 'super secret key'
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:vanarcar08@localhost:5432/mislucas"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
+#Conexión para consultas
+engine = create_engine('postgresql://postgres:vanarcar08@localhost:5432/mislucas')
+Session = sessionmaker(engine)
+session = Session()
 
 #Models
  
@@ -22,6 +29,7 @@ class User(db.Model):
     __tablename__ = "Usuarios"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(32), nullable=False)
+    surname = db.Column(db.String(32), nullable=False)
     email = db.Column(db.String(32), nullable=False)
     password = db.Column(db.String(32), nullable=False)
     total = db.Column(db.Integer(), nullable=False)
@@ -48,35 +56,64 @@ db.create_all()
 
 #Routes
 
-@app.route("/")
-def login():
-    return render_template("index.html")
+@app.route('/')
+def index():
+    return render_template('login.html')
 
-@app.route("/login")
-def registrar():
-    return render_template("login.html")
-
-@app.route("/registrar", methods=["POST"])
-def calculate_payment():
-    data=request.json
-    name = data["name"]
-    email = data["email"]
-    password = password["password"]
-    total = data["total"]
+@app.route('/usuario_login', methods= ['POST'])
+def login_todo():
+    usu= request.form.get('usuario', )
+    contra= request.form.get('contrasena', )
     
+    con = session.query(User).filter(
+        User.email == usu
+    ).filter(
+        User.password == contra
+    )
+    
+    if con.count() > 0:
+        return render_template('dashboard.html')
+    else:
+        return "El usuario no existe"
+    
+@app.route('/usuario_crear_registro', methods= ['POST'])
+def crear_usuario_todo():
+    corr= request.form.get('correo', )
+    nomb= request.form.get('nombre', )
+    apell= request.form.get('apellido', )
+    contra= request.form.get('contrasena', )
+    
+    session.add(User(email=corr,name=nomb,surname=apell,password=contra))
+    session.commit()
+    
+    flash('Usuario Creado')
+    
+    return render_template('register.html')
+    
+@app.route('/usuario_recuperar')
+def recuperar_todo():
+    return render_template('recuperar.html')
 
-    user = User(
-		name=name,
-		email=email,
-        password = password,
-		total=int(total),
-	)
+@app.route('/usuario_recuperar_contra', methods= ['POST'])
+def recuperar_contra_todo():
+    usu= request.form.get('usuario', )
+    
+    con = session.query(User.password).filter(
+        User.email == usu
+    )
+    
+    if con.count() > 0:
+        flash('Su contraseña es: ' + con[0].contrasena)
+    else: flash('El usuario no existe')
+    
+    return render_template('recuperar.html')
+        
 
-    db.session.add(user)
-    db.session.commit()
-    money = user.calculate_total()
-    return jsonify({"Monto total": money}), 200
+@app.route('/usuario_create')
+def create_todo():
+    return render_template('register.html')
 
 
+#Runner
 if __name__ == "__main__":
 	app.run(debug=True, port=5000)
